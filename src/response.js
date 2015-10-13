72a7fdcccff176c035669415901ade12f3516ff9
Dylan Piercey <pierceydylan@gmail.com>
2015-10-12 20:38:17 -0600

    Default status to 404

--- src/response.js
+++ src/response.js
@@ -4,8 +4,8 @@ function Response (ctx) {
 	var res  = ctx.res;
 	var self = this;
 
-	this.status  = res.statusCode || res.status || 404;
 	this.headers = res.headers = { "x-powered-by": "Rill" };
+	this.status  = 404;
 	this.body    = undefined;
 
 	res.once("finish", function () { self.finished = true; });
